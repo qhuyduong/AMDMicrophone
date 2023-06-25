f17fa95d6e9f381fd9eaa6693ed210dabc1ea112
Huy Duong <qhuyduong@hotmail.com>
2023-06-25 16:47:59 +0700

    Add template code
    Signed-off-by: Huy Duong <qhuyduong@hotmail.com>

--- AMDMicrophone/AMDMicrophone.cpp
+++ AMDMicrophone/AMDMicrophone.cpp
@@ -1,1 +1,8 @@
-/* add your code here */
+//
+//  AMDMicrophone.cpp
+//  AMDMicrophone
+//
+//  Created by Huy Duong on 25/06/2023.
+//
+
+#include "AMDMicrophone.hpp"
--- AMDMicrophone/AMDMicrophone.hpp
+++ AMDMicrophone/AMDMicrophone.hpp
@@ -1,1 +1,13 @@
-/* add your code here */
+//
+//  AMDMicrophone.hpp
+//  AMDMicrophone
+//
+//  Created by Huy Duong on 25/06/2023.
+//
+
+#ifndef AMDMicrophone_hpp
+#define AMDMicrophone_hpp
+
+#include <stdio.h>
+
+#endif /* AMDMicrophone_hpp */
