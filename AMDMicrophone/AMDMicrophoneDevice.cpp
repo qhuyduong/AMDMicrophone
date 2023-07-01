4c6b83a8d19455abcffdb6b736a40b83738255db
Huy Duong <qhuyduong@hotmail.com>
2023-07-01 18:06:35 +0700

    Add audio controls

--- AMDMicrophone/AMDMicrophoneDevice.cpp
+++ AMDMicrophone/AMDMicrophoneDevice.cpp
@@ -7,6 +7,10 @@
 
 #include "AMDMicrophoneDevice.hpp"
 
+#include <IOKit/audio/IOAudioDefines.h>
+#include <IOKit/audio/IOAudioLevelControl.h>
+#include <IOKit/audio/IOAudioToggleControl.h>
+
 #define super IOAudioDevice
 
 OSDefineMetaClassAndStructors(AMDMicrophoneDevice, IOAudioDevice);
@@ -15,6 +19,7 @@ bool AMDMicrophoneDevice::createAudioEngine()
 {
     bool result = false;
     AMDMicrophoneEngine* audioEngine = NULL;
+    IOAudioControl* control;
 
     LOG("createAudioEngine()\n");
 
@@ -27,13 +32,66 @@ bool AMDMicrophoneDevice::createAudioEngine()
         goto Done;
     }
 
-    activateAudioEngine(audioEngine);
-    audioEngine->release();
+    // Create a left & right input gain control with an int range from 0 to 65535
+    // and a db range from 0 to 22.5
+    control = IOAudioLevelControl::createVolumeControl(65535, // Initial value
+        0, // min value
+        65535, // max value
+        0, // min 0.0 in IOFixed
+        (22 << 16) + (32768), // 22.5 in IOFixed (16.16)
+        kIOAudioControlChannelIDDefaultLeft,
+        kIOAudioControlChannelNameLeft,
+        0, // control ID - driver-defined
+        kIOAudioControlUsageInput);
+    if (!control) {
+        goto Done;
+    }
+
+    control->setValueChangeHandler((IOAudioControl::IntValueChangeHandler)gainChangeHandler, this);
+    audioEngine->addDefaultAudioControl(control);
+    control->release();
+
+    control = IOAudioLevelControl::createVolumeControl(65535, // Initial value
+        0, // min value
+        65535, // max value
+        0, // min 0.0 in IOFixed
+        (22 << 16) + (32768), // max 22.5 in IOFixed (16.16)
+        kIOAudioControlChannelIDDefaultRight, // Affects right channel
+        kIOAudioControlChannelNameRight,
+        0, // control ID - driver-defined
+        kIOAudioControlUsageInput);
+    if (!control) {
+        goto Done;
+    }
+
+    control->setValueChangeHandler((IOAudioControl::IntValueChangeHandler)gainChangeHandler, this);
+    audioEngine->addDefaultAudioControl(control);
+    control->release();
+
+    // Create an input mute control
+    control = IOAudioToggleControl::createMuteControl(false, // initial state - unmuted
+        kIOAudioControlChannelIDAll, // Affects all channels
+        kIOAudioControlChannelNameAll,
+        0, // control ID - driver-defined
+        kIOAudioControlUsageInput);
+
+    if (!control) {
+        goto Done;
+    }
+
+    control->setValueChangeHandler((IOAudioControl::IntValueChangeHandler)inputMuteChangeHandler, this);
+    audioEngine->addDefaultAudioControl(control);
+    control->release();
+
+    if (activateAudioEngine(audioEngine) != kIOReturnSuccess) {
+        LOG("ERROR activateAudioEngine failed\n");
+        goto Done;
+    }
 
     result = true;
 
 Done:
-    if (!result && (audioEngine != NULL)) {
+    if (audioEngine != NULL) {
         audioEngine->release();
     }
 
@@ -100,4 +158,51 @@ void AMDMicrophoneDevice::free()
     }
 
     super::free();
+}
+
+IOReturn AMDMicrophoneDevice::gainChangeHandler(IOService* target, IOAudioControl* gainControl, SInt32 oldValue, SInt32 newValue)
+{
+    IOReturn result = kIOReturnBadArgument;
+    AMDMicrophoneDevice* audioDevice;
+
+    audioDevice = (AMDMicrophoneDevice*)target;
+    if (audioDevice) {
+        result = audioDevice->gainChanged(gainControl, oldValue, newValue);
+    }
+
+    return result;
+}
+
+IOReturn AMDMicrophoneDevice::gainChanged(IOAudioControl* gainControl, SInt32 oldValue, SInt32 newValue)
+{
+    LOG("gainChanged(%d, %d)\n", oldValue, newValue);
+
+    if (gainControl) {
+        LOG("\t-> Channel %d\n", gainControl->getChannelID());
+    }
+
+    // Add hardware gain change code here
+    return kIOReturnSuccess;
+}
+
+IOReturn AMDMicrophoneDevice::inputMuteChangeHandler(IOService* target, IOAudioControl* muteControl, SInt32 oldValue, SInt32 newValue)
+{
+    IOReturn result = kIOReturnBadArgument;
+    AMDMicrophoneDevice* audioDevice;
+
+    audioDevice = (AMDMicrophoneDevice*)target;
+    if (audioDevice) {
+        result = audioDevice->inputMuteChanged(muteControl, oldValue, newValue);
+    }
+
+    return result;
+}
+
+IOReturn AMDMicrophoneDevice::inputMuteChanged(IOAudioControl* muteControl, SInt32 oldValue, SInt32 newValue)
+{
+    LOG("inputMuteChanged(%d, %d)\n", oldValue, newValue);
+
+    // Add input mute change code here
+
+    return kIOReturnSuccess;
 }
